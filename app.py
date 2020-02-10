fe5e465f53baf75fe55e63738363b2aef4017f99
Pheyonix <dev@vishnudileesh.com>
2020-02-10 18:07:36 +0530

    Grab data from ipgeolocation.io api

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from config import API_KEY, IP
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
+    #ip = request.remote_addr
+
+    r = requests.get('https://api.ipgeolocation.io/astronomy?apiKey=' + API_KEY + '&ip=' + IP)
+
+    print(r.content)
+
 
     return "Hello World"
 
